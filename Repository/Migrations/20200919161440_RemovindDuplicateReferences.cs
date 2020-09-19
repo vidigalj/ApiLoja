4e9c6976894b9258af5adb351c1957d37b2ab982
JeanVidigal <jeanvidigal@gmail.com>
2020-09-19 13:15:09 -0300

    IncludingMigrations

---
+++ Repository/Migrations/20200919161440_RemovindDuplicateReferences.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class RemovindDuplicateReferences : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "ProductValue",
+                table: "OrderItem");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "OrderItem",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ProductValue",
+                table: "OrderItem",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
